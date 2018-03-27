e705f4520e5bd3c18334be1a08f589e9aaf7599e
Sample Coder <samp_coder@yahoo.com>
2018-03-28 02:48:18 +0300

    Fix tabs.

--- testpq.cpp
+++ testpq.cpp
@@ -8,18 +8,18 @@ int main()
 {
     PQClient pgClient("host=localhost port=5432 user=postgres dbname=postgres password=123");
     if (pgClient.Connect()) {
-		bool ok;
-		std::cout << "Connection OK.\n";
-		ok = pgClient.Exec(q1);
-		if (ok) {
-			ok = pgClient.Exec(q2);
-		}
-		if (!ok) {
-			std::cout << pgClient.ResultErrorMessage();
-		}
+        bool ok;
+        std::cout << "Connection OK.\n";
+        ok = pgClient.Exec(q1);
+        if (ok) {
+            ok = pgClient.Exec(q2);
+        }
+        if (!ok) {
+            std::cout << pgClient.ResultErrorMessage();
+        }
     }
     else
-		std::cout << "Got error: " << pgClient.ErrorMessage();
+        std::cout << "Got error: " << pgClient.ErrorMessage();
     return 0;
 }
 
